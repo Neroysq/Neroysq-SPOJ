fddc5cc671a88cba4407b0cad66ad53f45fce588
Neroysq <yaosiqiu@gmali.com>
2013-01-05 17:05:18 +0800

    Code update
    PALIN code updated

--- Codes/PALIN.cpp
+++ Codes/PALIN.cpp
@@ -1,36 +1,54 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstring>
 const int nmax = 1000000;
 
-int t, l;
-bool get;
 char str[nmax + 18];
+int n, T;
 
 int main()
 {
-    freopen("PALIN.in", "r", stdin);
-    freopen("PALIN.out", "w", stdout);
-    scanf("%d\n", &t);
-    while (t--)
-    {
-	get = false;
-	scanf("%s\n", str + 1);
-	len = strlen(str);
-	if (len == 1)
-	{
-	    printf("%s", str[1] == '9' ? "11" : str + 1);
-	    continue;
+  freopen("PALIN.in", "r", stdin);
+  freopen("PALIN.txt", "w", stdout);
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%s", str + 1);
+    n = strlen(str + 1);
+    bool jump = 1;
+    for (int i = 1; i <= n; ++i)
+      if (str[i] != '9') {
+	jump = 0;
+	break;
+      }
+    if (jump) {
+      printf("1");
+      for (int i = 1; i < n; ++i) printf("0");
+      printf("1\n");
+    }
+    else {
+      int pos = 0;
+      for (int i = n >> 1; i; --i)
+	if (str[i] != str[n - i + 1]) {
+	  pos = i;
+	  break;
 	}
-	for (int l = 1, r = l, ln, rn; l < r; ++l, --r)
-	{
-	    ln = str[l] - '0';
-	    rn = str[r] - '0';
-	    if (ln != 9 || rn != 9) get = true;
-	}
-	if (!get)
-	{
-	    printf("1");
-	    for (int i = 1; i < len; ++i)
-		printf("0");
-	    printf("1\n");
-	}
-	for (int l = len >> 1
+      if (pos && str[pos] > str[n - pos + 1]) 
+	for (int i = pos; i; --i)
+	  str[n - i + 1] = str[i];
+      else {
+	int np = 0;
+	for (int i = (n + 1) >> 1; i; --i)
+	  if (str[i] < '9') {
+	    np = i;
+	    break;
+	  }
+	++str[np];
+	for (int i = (n + 1) >> 1; i > np; --i)
+	  str[i] = str[n - i + 1] = '0';
+	for (int i = np; i; --i) str[n - i + 1] = str[i];
+      }
+      printf("%s\n", str + 1);
+    }
+  }
+  return 0;
+}
+
